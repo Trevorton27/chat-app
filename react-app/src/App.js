99e9586fd07ebf4833cdecdc28e4c0e2ef8edafb
Trevor Mearns <spiral272@gmail.com>
2019-04-25 18:37:07 +0900

    chatkit functionality established but data is not being sent or received. Documentation is not helpful here. May just focus on react functionality and how to connect with a database over chatkit :-)

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -12,7 +12,16 @@ import { tokenUrl, instanceLocator } from './config';
 
 class App extends Component {
 
+  constructor() {
+    super()
+    this.state = {
+      messages: []
+    }
+  }
+
   componentDidMount() {
+
+    console.log("componentDidMount function fired :-)");
 
     const tokenProvider = new TokenProvider({
       url: tokenUrl
@@ -28,20 +37,33 @@ class App extends Component {
 
     chatManager.connect()
     .then(currentUser => {
-      currentUser.subscribeToRoom({
-        roomId: 19396680,
+      currentUser.subscribeToRoomMultipart({
+        roomId: '19396680',
         hooks: {
           onNewMessage: message => {
-            console.log('message.text: ', message.text);
+            console.log('message.text: ', message.parts);
+            this.setState({
+              messages: [ ...this.state.messages, message.parts]
+            })
           }
         }
       })
     })
+
+    chatManager.connect()
+  .then(currentUser => {
+    console.log('Successful connection', currentUser)
+  })
+  .catch(err => {
+    console.log('Error on connection', err)
+  })
   }
+
   render() {
+    console.log('this.state.messages: ', this.state.messages);
     return (
       <div className="App">
-        <MessageList />
+        <MessageList messages={this.state.messages} />
       </div>
     );
   }
--- react-app/src/components/MessageList.js
+++ react-app/src/components/MessageList.js
@@ -19,7 +19,7 @@ class MessageList extends React.Component {
     render() {
         return (
             <div className="message-list">
-              {DUMMY_DATA.map((message, index) => {
+              {this.props.messages.map((message, index) => {
                   return (
                       <div key={index} className="message">
                         <div className="message-text">{message.senderId}</div>
