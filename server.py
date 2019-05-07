9a1eb3a1710a77eb846d11c18e65ed43b056abb0
Trevor Mearns <spiral272@gmail.com>
2019-05-07 13:50:45 +0900

    issue with configuring routing for flask to run react app

--- server.py
+++ server.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from flask import send_from_directory
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/react-app/path:index.html')
+def download_file(index):
+    return send_from_directory('react-app', 'build', 'static')
 
 if __name__ == "__main__":
     app.run()
