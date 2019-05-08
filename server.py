2ecb700ac4402a25a54260773e501b23aee9d6ed
Trevor Mearns <spiral272@gmail.com>
2019-05-08 14:27:18 +0900

    no headway made in routing react app through flask. Documentation is extremely vague for me at this point in my development

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask import send_from_directory
+
 app = Flask(__name__)
 
-@app.route('/react-app/path:index.html')
-def download_file(index):
-    return send_from_directory('react-app', 'build', 'static')
+
+@app.route('/uploads/<path:/react-app/build/index.html>')
+def download_file():
+   return send_from_directory(app.config['UPLOAD_FOLDER'],
+    'index.html', as_attachment=True )
+
+
 
 if __name__ == "__main__":
     app.run()
