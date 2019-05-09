a4b895ac932418034f87dc9ed2973139acd1c018
Trevor Mearns <spiral272@gmail.com>
2019-05-09 13:17:58 +0900

    test commit

--- server.py
+++ server.py
@@ -1,13 +1,23 @@
 from flask import Flask
 from flask import send_from_directory
+from flask import render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="react-app/build/static",
+template_folder="build")
 
+
+#@app.route('/react-app/build//index.html')
+#def hello():
+   #return send_from_directory('react-app/build',
+    #'index.html' )
 
 @app.route('/')
-def hello():
-   return send_from_directory('/build', 'index.html')
+def index():
+    return render_template('index.html')
 
+print('Starting Flask. Yay!')
+
+app.debug=True
 
 
 if __name__ == "__main__":
