8b13853dbaa12b6f58841c9b95012337c09c6f80
Trevor Mearns <spiral272@gmail.com>
2019-05-14 14:55:22 +0900

    made some progress integrating postgres with the app then blew it :-)

--- server.py
+++ server.py
@@ -1,15 +1,12 @@
 from flask import Flask
 from flask import send_from_directory
 from flask import render_template
+import psycopg2
 
-app = Flask(__name__, static_folder="react-app/build/static",
-template_folder="build")
-
-
-#@app.route('/react-app/build//index.html')
-#def hello():
-   #return send_from_directory('react-app/build',
-    #'index.html' )
+app = Flask(__name__,
+    static_folder="react-app/build/static",
+    template_folder="react-app/build"
+    )
 
 @app.route('/')
 def index():
@@ -17,8 +14,31 @@ def index():
 
 print('Starting Flask. Yay!')
 
-app.debug=True
+@app.route('/messages', method=['POST'])
 
+@app.route('/retrieve-messages', method=['GET'])
+
+try:
+    connection = psycopg2.connect(user = "postgres",
+                                  password = "banana",
+                                  host = "localhost",
+                                  port = "5432",
+                                  database = "postgres")
+    cursor = connection.cursor()
+    # Print PostgreSQL Connection properties
+    print ( connection.get_dsn_parameters(),"\n")
+    # Print PostgreSQL version
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    print("You are connected to - ", record,"\n")
+except (Exception, psycopg2.Error) as error :
+    print ("Error while connecting to PostgreSQL", error)
+finally:
+    #closing database connection.
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
 
 if __name__ == "__main__":
     app.run()
