eff3747cca5a9707936266d121e8c1a4c36e71ad
Trevor Mearns <spiral272@gmail.com>
2019-05-16 16:01:30 +0900

    database connection added but have run into obstacles

---
+++ database_service.py
@@ -0,0 +1,31 @@
+import psycopg2
+
+def open_connection(sql_statement):   
+    try:
+        connection = psycopg2.connect(user = "postgres",
+                                    password = "banana",
+                                    host = "localhost",
+                                    port = "5432",
+                                    database = "postgres")
+        cursor = connection.cursor()
+        # Print PostgreSQL Connection properties
+        print ( connection.get_dsn_parameters(),"\n")
+        # Print PostgreSQL version
+        cursor.execute(sql_statment)
+        record = cursor.fetchone()
+        print('record type: %s' %type(record[0]))
+        print("You are connected to - ", record,"\n")
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while connecting to PostgreSQL", error)
+    finally:
+        #closing database connection.
+            if(connection):
+                cursor.close()
+                connection.close()
+                print("PostgreSQL connection is closed")
+
+def get_messages():
+    return open_connection("SELECT version()")
+
+def get_users():
+    return open_connection("SELECT * FROM users")
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import send_from_directory
 from flask import render_template
 import psycopg2
+from database_service import open_connection
 
 app = Flask(__name__,
     static_folder="react-app/build/static",
@@ -14,31 +15,22 @@ def index():
 
 print('Starting Flask. Yay!')
 
-@app.route('/messages', method=['POST'])
+#@app.route('/api/messages', method=['GET', 'POST'])
+#def messages():
+    #if GET request
+    #return all users
+    #users = list()
+    #list.append({
+    ## "username": "I Am Batman"
+    # })
+     #return json data for list
 
-@app.route('/retrieve-messages', method=['GET'])
+    #if POST reques
+    #create a user
+    
 
-try:
-    connection = psycopg2.connect(user = "postgres",
-                                  password = "banana",
-                                  host = "localhost",
-                                  port = "5432",
-                                  database = "postgres")
-    cursor = connection.cursor()
-    # Print PostgreSQL Connection properties
-    print ( connection.get_dsn_parameters(),"\n")
-    # Print PostgreSQL version
-    cursor.execute("SELECT version();")
-    record = cursor.fetchone()
-    print("You are connected to - ", record,"\n")
-except (Exception, psycopg2.Error) as error :
-    print ("Error while connecting to PostgreSQL", error)
-finally:
-    #closing database connection.
-        if(connection):
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
+open_connection(sql_statement)
+
 
 if __name__ == "__main__":
     app.run()
