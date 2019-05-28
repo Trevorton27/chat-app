0bf72185799183994694f1cf43138c7eb719026d
Trevor Mearns <spiral272@gmail.com>
2019-05-28 20:10:14 +0900

    chatkit funtionality removed and axios installed to enable data flow into react-app  directory

--- react-app/src/components/MessageList.js
+++ react-app/src/components/MessageList.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import Message from './Message';
-
+import axios from 'axios';
 
 
 class MessageList extends React.Component {
+
+    getMessages() {
+        console.log('sendMessage function was run');
+        this.setState(
+         axios.get('/api/messages').then(function (response) {
+           console.log(response);
+         })
+          )
+      }
+
     render() {
         return (
             <div className="message-list">
