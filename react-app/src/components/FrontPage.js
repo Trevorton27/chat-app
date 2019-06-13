1889a85c276387ffce2a2fbd2c0db761e73408b0
Trevor Mearns <spiral272@gmail.com>
2019-06-13 15:09:36 +0900

    built skeleton for front page and installed react-router-dom

---
+++ react-app/src/components/FrontPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+class FrontPage extends React.component {
+
+    render() {
+        return(
+            <div className="main container">
+                
+            </div>
+        )
+    }
+}
+
+export default FrontPage
