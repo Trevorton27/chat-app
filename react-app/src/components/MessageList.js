076c197e4cb97ea08b9d4215cd9f05324662cc3f
Trevorton27 <spiral272@gmail.com>
2019-07-31 12:23:11 +0900

    deleted package.json and node_modules folder from main directory

--- react-app/src/components/MessageList.js
+++ react-app/src/components/MessageList.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Message from './Message';
-import axios from 'axios';
 
 
 
@@ -8,13 +7,8 @@ import axios from 'axios';
 
 class MessageList extends React.Component {
 
-  axios.get('api/users?user_id=' + userId)
-  .then((response) => {
-    this.setState({
-      currentUser: response.data
-    })
-  })
   
+
   render() {
     
         return (
@@ -22,14 +16,8 @@ class MessageList extends React.Component {
             <div className="message-list">
               {this.props.messages.map((message, index) => {
                 const messageComponent = <Message className="col" key={index} username={message.username} text={message.text} />;
-                const currentUser = localStorage.setItem('chat-user-id', response.data.userId)
-                this.setState({
-                  currentUser: {
-                    username: username,
-                    id: response.data
-                  }
-                });
-                if(message.username === currentUser) {
+             
+                if(message.username === 'SomeGuy') {
                   return (
                     <div className="row">
                       <div className="col"></div>
